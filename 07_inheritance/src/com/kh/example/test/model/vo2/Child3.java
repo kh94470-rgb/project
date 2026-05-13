8b11e39489fffb5ad326703e1c3daf25d0ec5d33
kh94470-rgb <kh94470@gmail.com>
2026-05-13 17:40:56 +0900

    Add files via upload

---
+++ 07_inheritance/src/com/kh/example/test/model/vo2/Child3.java
@@ -0,0 +1,14 @@
+package com.kh.example.test.model.vo2;
+
+import com.kh.example.test.model.vo.Parent;
+
+public class Child3 extends Parent {
+	
+	public Child3() {
+//		super.num = -7;
+//		super.dNum = 45.2
+		super.bool = false;
+		super.ch = 'a';
+	}
+
+}
